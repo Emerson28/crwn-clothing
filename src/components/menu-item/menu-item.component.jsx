e351eee1cbebbc7f82a0680f04512db2450c5402
Ralph Emerson Almarez <ralph.almarez@octaltech.net>
2019-12-05 14:43:55 +0800

    update our menu-items to navigate withRouter HOC

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+/* withRouter is a higher order component or (HOC) it is essentially a
+ function that takes a component as an argument and which turn you a modified component
+ a higher order component is a function that takes any component and modifies it in some way and then returns you that
+ new modified component or a super powered component*/
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${ size } menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div className={`${ size } menu-item`} onClick={ () => history.push(`${match.url}${linkUrl}`) }>
     <div className="background-image" style={{ backgroundImage: `url(${ imageUrl })` }} />
         <div className="content">
           <h1 className="title">{ title.toUpperCase() }</h1>
@@ -12,4 +17,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
